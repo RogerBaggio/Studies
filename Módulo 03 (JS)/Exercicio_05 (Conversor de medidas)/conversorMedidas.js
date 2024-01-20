9eb90f45636e7f8b4a6f5651c6c53b51538e3713
Roger Baggio <biasolibaggio@hotmail.com>
2024-01-19 22:06:14 -0300

    Desenvolvimento parcial do conversor de medidas

--- Módulo 03 (JS)/Exercicio_05 (Conversor de medidas)/conversorMedidas.js
+++ Módulo 03 (JS)/Exercicio_05 (Conversor de medidas)/conversorMedidas.js
@@ -1,23 +1,92 @@
 console.log("Executando JS...");
 
-const medida = coletarValor();
+// Main Script>
 
-function coletarValor() {
-  let medida = prompt("Insira o valor de metragem a ser convertida:");
+const measure = collectValue();
+console.log("'"+measure+"'");
 
-  while (isNaN(medida)) {
-    medida = prompt(
-      "Insira o valor de metragem a ser convertida:\n(Utilize apenas apenas números, por favor.):"
+const option = optionSelect();
+
+const convertedMeasureMsg = measureConversor(measure, option);
+
+alert(convertedMeasureMsg);
+
+// Functions >
+
+function collectValue() {
+  let value = prompt("Insira o valor em METROS a ser convertido:");
+
+  while (isNaN(value) || value <= 0) {
+    value = prompt(
+      "Insira o valor em METROS a ser convertido:\n(Utilize apenas apenas números e maiores que zero, por favor.):"
     );
   }
-  return medida;
+  return value.trim();
 }
-/*metro para milimetro *1000
-metro para centimetro *100
-metro para decimetros *10
-metro para decametros /10
-metro para hectometros /100
-metro para quilometros /1000
 
+function optionSelect(){
+  let opt = prompt("Selecione uma opção de medida para qual o valor deverá ser convertido:"+
+    "\n 1 - converter para MILIMETROS. (*1000)"+
+    "\n 2 - converter para CENTIMETROS. (*100)"+
+    "\n 3 - converter para DECIMETROS. (*10)"+
+    "\n 4 - converter para DECAMETROS. (/10)"+
+    "\n 5 - converter para HECTOMETROS. (/100)"+
+    "\n 6 - converter para QUILOMETROS. (/1000)"
+  ).trim();
 
-*/
+  console.log("Before while - opt value: '"+opt+ "' typeof opt: "+typeof(opt));
+
+  while ((isNaN(opt)) || Number(opt) <= 0 || Number(opt) >= 7 || opt.length === 0) {
+    
+    console.log("Inside while - opt value: '"+opt+ "' typeof opt: "+typeof(opt));
+    
+    selection = prompt("OPÇÃO INSERIDA É INVALIDA."+
+      "\nSelecione uma opção de medida para qual o valor deverá ser convertido:"+
+      "\n 1 - converter para MILIMETROS. (*1000)"+
+      "\n 2 - converter para CENTIMETROS. (*100)"+
+      "\n 3 - converter para DECIMETROS. (*10)"+
+      "\n 4 - converter para DECAMETROS. (/10)"+
+      "\n 5 - converter para HECTOMETROS. (/100)"+
+      "\n 6 - converter para QUILOMETROS. (/1000)"
+    );
+  }
+
+  return Number(opt);
+}
+
+function measureConversor(value, selection){
+let convertedValueMessage;
+
+  switch (Number(selection)) {
+    case 1:
+      convertedValueMessage = value * 1000 +" milimetros";
+      console.log("MILIMETROS selecionado (* 1000)");
+      break;
+    case 2:
+      convertedValueMessage = value * 100 +" centimetros";
+      console.log("CENTIMETROS selecionado (* 100)");
+      break;
+    case 3:
+      convertedValueMessage = value * 10 +" decimetros";
+      console.log("DECIMETROS selecionado (* 10)");
+      break;
+    case 4:
+      convertedValueMessage = value / 10 +" decametros";
+      console.log("DECAMETROS selecionado (/ 10)");
+      break;
+    case 5:
+      convertedValueMessage = value / 100 +" hecmetros";
+      console.log("HECTOMETROS selecionado (/ 100)");
+      break;
+    case 6:
+      convertedValueMessage = value / 1000 +" quilometros";
+      console.log("QUILOMETROS selecionado (/ 1000)");
+      break;
+  }
+
+  console.log("Value :"+ value+". Converted value: "+convertedValueMessage);
+
+  return convertedValueMessage;
+}
+
+console.log("JS carregado com sucesso.");
