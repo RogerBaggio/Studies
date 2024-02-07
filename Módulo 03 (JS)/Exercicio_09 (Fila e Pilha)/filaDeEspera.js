be2b42c5466a2a3c7665a50677e12364b7247605
Roger Baggio <biasolibaggio@hotmail.com>
2024-02-07 07:13:11 -0300

    Iciado resolução de exercicio09 Filas e Pilhas.

---
+++ Módulo 03 (JS)/Exercicio_09 (Fila e Pilha)/filaDeEspera.js
@@ -0,0 +1,25 @@
+console.log("Executando JS...");
+console.log("Fila de Espera.");
+
+// Main Script >
+alert(
+  "Bem vindo ao exercício Fila de Espera em JS!\n\n" +
+    "Adicione aqui os nomes dos pacientes em ordem de chegada." +
+    "\n\nNo menu seguinte, você poderá escolher entre atender o próximo paciente ou adicionar um novo paciente a fila de atendimentos."
+);
+
+var patientList = [];
+
+//TODO:Prompt menu - Adicionar paciente, Atender paciente, Exibir fila, Sair
+
+//TODO: Adicionar paciente - checar se prompt vazio. Cancel volta para menu.
+
+//TODO: Atender paciente - exibir nome. Se lista vazia, voltar para menu.
+
+//TODO: Exibir fila - Listar nomes da lista (1° - Nome, 2°...). Se fila vazia, voltar para menu.
+
+//TODO: Sair - Mensagem de despedida. Fim de ciclo.
+
+// Functions >
+
+console.log("JS executado com sucesso.");
---
+++ Módulo 03 (JS)/Exercicio_09 (Fila e Pilha)/pilhaDeCartas.js
@@ -0,0 +1,8 @@
+console.log("Executando JS...");
+console.log("Pilha de Cartas.");
+
+// Main Script >
+
+// Functions >
+
+console.log("JS executado com sucesso."); 
