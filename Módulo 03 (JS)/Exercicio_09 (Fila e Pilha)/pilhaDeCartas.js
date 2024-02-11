55174f16df24051c717cedd00029f162c3a2d077
Roger Baggio <biasolibaggio@hotmail.com>
2024-02-10 22:48:22 -0300

    Iniciado desenvolvimento de exercicio 09 B Pilha de Cartas.

--- Módulo 03 (JS)/Exercicio_09 (Fila e Pilha)/pilhaDeCartas.js
+++ Módulo 03 (JS)/Exercicio_09 (Fila e Pilha)/pilhaDeCartas.js
@@ -3,6 +3,87 @@ console.log("Pilha de Cartas.");
 
 // Main Script >
 
+const cardList = [
+  "As de Ouros ♦",
+  "As de Espadas ♠",
+  "As de Copas ♥",
+  "As de Paus ♣",
+  "2 de Ouros ♦",
+  "2 de Espadas ♠",
+  "2 de Copas ♥",
+  "2 de Paus ♣",
+  "3 de Ouros ♦",
+  "3 de Espadas ♠",
+  "3 de Copas ♥",
+  "3 de Paus ♣",
+  "4 de Ouros ♦",
+  "4 de Espadas ♠",
+  "4 de Copas ♥",
+  "4 de Paus ♣",
+  "5 de Ouros ♦",
+  "5 de Espadas ♠",
+  "5 de Copas ♥",
+  "5 de Paus ♣",
+  "6 de Ouros ♦",
+  "6 de Espadas ♠",
+  "6 de Copas ♥",
+  "6 de Paus ♣",
+  "7 de Ouros ♦",
+  "7 de Espadas ♠",
+  "7 de Copas ♥",
+  "7 de Paus ♣",
+  "8 de Ouros ♦",
+  "8 de Espadas ♠",
+  "8 de Copas ♥",
+  "8 de Paus ♣",
+  "9 de Ouros ♦",
+  "9 de Espadas ♠",
+  "9 de Copas ♥",
+  "9 de Paus ♣",
+  "10 de Ouros ♦",
+  "10 de Espadas ♠",
+  "10 de Copas ♥",
+  "10 de Paus ♣",
+  "J de Ouros ♦",
+  "J de Espadas ♠",
+  "J de Copas ♥",
+  "J de Paus ♣",
+  "Q de Ouros ♦",
+  "Q de Espadas ♠",
+  "Q de Copas ♥",
+  "Q de Paus ♣",
+  "K de Ouros ♦",
+  "K de Espadas ♠",
+  "K de Copas ♥",
+  "K de Paus ♣",
+];
+
+alert(
+  "Bem vindo ao Exerício 09 B - Pilha de Cartas!\n\n" +
+    "Este sistema não serve pra nada além de exemplificar algumas manipulações básicas de Arrays em JavaScript.\n\n" +
+    "Confira as operações possíveis no menu seguinte."
+);
+
+/*TODO: Menu inicial mostrando qtd cartas no baralho, qtd cartas sacadas e menu com opções:
+ 1 listar cartas do baralho
+ 2 listar cartas sacadas
+ 3 puxar carta aleatoria do baralho
+ 4 devolver carta selecionada ao baralho
+ 5 sair do sistema.
+*/
+
+//TODO: opção 1 - Exibir lista de cartas no baralho.
+
+//TODO: opção 2 - Exibir lista de cartas na sacadas.
+
+//TODO: opção 3 - Sacar carta aleatoria do baralho.
+
+//TODO: opção 4 - Devolver carta selecionada ao baralho.
+
+//TODO: opção 5 - Sair.
+
+alert("Ok, bye.\n\nObrigado e até a próxima!");
+
 // Functions >
 
 console.log("JS executado com sucesso.");
