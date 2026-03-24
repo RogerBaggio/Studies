9f0d8a177c77ec0961d671b22a291e9d3822fab2
RogerBaggio <RogerBaggio@users.noreply.github.com>
2026-03-24 11:10:17 -0300

    First basic endpoint created.

---
+++ Spring Boot LAB/spring-lab/src/main/java/com/rojudo/spring_lab/service/UserService.java
@@ -0,0 +1,24 @@
+package com.rojudo.spring_lab.service;
+
+import com.rojudo.spring_lab.model.UserDTO;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
+public class UserService {
+    
+    private final List<UserDTO> users = new ArrayList<>();
+    private Long nextId = 1L;
+    
+    public List<UserDTO> findAll() {
+        return new ArrayList<>(users);
+    }
+    
+    public UserDTO create(String name, String email) {
+        // Aqui vão regras de negócio (Validar se único, verificar se vazio, regras de formatação...)       
+        UserDTO newUser = new UserDTO(nextId++, name, email);
+        users.add(newUser);
+        return newUser;
+    }
+}
